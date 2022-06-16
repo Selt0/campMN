330184c7e02646dc06bbc815084d6ad607ba030b
Selt0 <chewedgum12@gmail.com>
2022-06-16 12:20:39 -0500

    Use path join on public

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.engine('ejs', engine)
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
