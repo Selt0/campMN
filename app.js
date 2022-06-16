fdc4c65c7fd541098007f487f65175c67277d9e6
Selt0 <chewedgum12@gmail.com>
2022-06-16 12:16:31 -0500

    Moved function in boilerplate to own file in public dir

--- app.js
+++ app.js
@@ -19,10 +19,13 @@ mongoose.connection.once('open', () => {
 })
 
 app.set('view engine', 'ejs')
-app.engine('ejs', engine)
 app.set('views', path.join(__dirname, 'views'))
+
+app.engine('ejs', engine)
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static('public'))
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
