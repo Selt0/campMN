55e67ada0c177cf43004709b1f95887f9a559ca8
Selt0 <chewedgum12@gmail.com>
2022-06-17 11:06:44 -0500

    move middleware to one file

--- middleware.js
+++ middleware.js
@@ -1,9 +1,34 @@
+const ExpressError = require('./utils/ExpressError')
+const { campgroundSchema } = require('./schemas')
+const Campground = require('./models/campground')
+
 module.exports.isLoggedIn = (req, res, next) => {
 	if (!req.isAuthenticated()) {
 		req.session.returnTo = req.originalUrl
-		console.log('saved!')
 		req.flash('error', 'you must be signed in')
 		return res.redirect('/login')
 	}
+	next()
+}
+
+module.exports.validateCampgorund = (req, res, next) => {
+	const { error } = campgroundSchema.validate(req.body)
+	if (error) {
+		const msg = error.details.map(el => el.message).join(',')
+		throw new ExpressError(msg, 400)
+	} else {
+		next()
+	}
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+	const { id } = req.params
+	const campground = await Campground.findById(id)
+
+	if (!campground.author.equals(req.user._id)) {
+		req.flash('error', "You don't have permission to do that.")
+		res.redirect(`/campgrounds/${id}`)
+	}
+
 	next()
 }
