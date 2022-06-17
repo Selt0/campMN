e5ad2df6fcb9837538d50592fd619f42d5d3016e
Selt0 <chewedgum12@gmail.com>
2022-06-17 11:06:21 -0500

    Add author to be seedsfile when populating db with seeds

--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async () => {
 		const random1000 = Math.floor(Math.random() * 1000)
 		const price = Math.floor(Math.random() * 25) + 10
 		const camp = new Campground({
+			author: '62ac9ebca5cfc060d42afa11', // seed file account
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
 			image: 'https://source.unsplash.com/collection/483251',
