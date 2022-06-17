551a709fd9035d714e2269c6be9c4fa00e3e3e86
Selt0 <chewedgum12@gmail.com>
2022-06-17 11:39:49 -0500

    Add authorization to reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const router = express.Router({ mergeParams: true })
+const { isLoggedIn, isReviewAuthor } = require('../middleware')
 const Campground = require('../models/campground')
 const Review = require('../models/review')
 const catchAsync = require('../utils/catchAsync')
 
 router.post(
 	'/',
+	isLoggedIn,
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id)
 		const review = new Review(req.body.review)
+		review.author = req.user._id
 		campground.reviews.push(review)
 		await review.save()
 		await campground.save()
@@ -19,6 +22,8 @@ router.post(
 
 router.delete(
 	'/:reviewId',
+	isLoggedIn,
+	isReviewAuthor,
 	catchAsync(async (req, res) => {
 		const { id, reviewId } = req.params
 		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
