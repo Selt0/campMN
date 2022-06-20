37c2c92caf4ab4cc3c18d6b74bef367c7b52430f
Selt0 <chewedgum12@gmail.com>
2022-06-20 09:59:38 -0500

    Add basic security handling

--- app.js
+++ app.js
@@ -9,20 +9,22 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const engine = require('ejs-mate')
 const session = require('express-session')
+const mongoSanitize = require('express-mongo-sanitize')
 const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-
+const MongoDBStore = require('connect-mongo')
+const helmet = require('helmet')
 const ExpressError = require('./utils/ExpressError')
 const User = require('./models/user')
 
 const usersRoutes = require('./routes/users')
 const campgroundsRoutes = require('./routes/campgrounds')
 const reviewsRoutes = require('./routes/reviews')
+const dbUrl = process.env.DB_URL
+// 'mongodb://localhost:27017/camp'
 
-mongoose
-	.connect('mongodb://localhost:27017/camp')
-	.catch(err => console.err(err))
+mongoose.connect(dbUrl).catch(err => console.err(err))
 
 mongoose.connection.once('open', () => {
 	console.log('Database connected!')
@@ -31,21 +33,85 @@ mongoose.connection.once('open', () => {
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.engine('ejs', engine)
-
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(methodOverride('_method'))
+app.use(mongoSanitize())
+
+const store = new MongoDBStore({
+	mongoUrl: dbUrl,
+	secret: 'keyboard',
+	touchAfter: 24 * 60 * 60
+})
+
+store.on('error', e => {
+	console.log('SESSION STORE ERROR', e)
+})
+
 app.use(
 	session({
+		store,
 		secret: 'keyboard',
 		resave: false,
 		saveUninitialized: true,
 		cookie: {
+			// secure: true,
 			expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
 			maxAge: 1000 * 60 * 60 * 24 * 7
 		}
 	})
 )
+app.use(
+	helmet({
+		crossOriginEmbedderPolicy: false
+	})
+)
+
+const scriptSrcUrls = [
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://api.mapbox.com/',
+	'https://kit.fontawesome.com/',
+	'https://cdnjs.cloudflare.com/',
+	'https://cdn.jsdelivr.net'
+]
+const styleSrcUrls = [
+	'https://kit-free.fontawesome.com/',
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.mapbox.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://fonts.googleapis.com/',
+	'https://use.fontawesome.com/',
+	'https://cdn.jsdelivr.net'
+]
+const connectSrcUrls = [
+	'https://api.mapbox.com/',
+	'https://a.tiles.mapbox.com/',
+	'https://b.tiles.mapbox.com/',
+	'https://events.mapbox.com/'
+]
+const fontSrcUrls = []
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: ["'self'", ...connectSrcUrls],
+			scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+			styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+			workerSrc: ["'self'", 'blob:'],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				'blob:',
+				'data:',
+				'https://res.cloudinary.com/dwl4y3kmp/',
+				'https://images.unsplash.com/'
+			],
+			fontSrc: ["'self'", ...fontSrcUrls]
+		}
+	})
+)
+
 app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
